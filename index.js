55ad39c26c9942c2ea297d657e45ec2ad8d4f1ee
johnealves <johne_af@hotmail.com>
2021-07-17 11:04:05 -0300

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const app = require('./api/app');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
 
 app.listen(PORT, () => console.log(`liesten at port ${PORT}`));
