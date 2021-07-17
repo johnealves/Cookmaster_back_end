4083ca4b94b99e93bcb266193fb73f74246c52d7
johnealves <johne_af@hotmail.com>
2021-07-17 11:12:13 -0300

    feat cors

--- api/app.js
+++ api/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const controllers = require('../controllers/index');
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
 
 app.get('/', (req, res) =>{
   res.send('Vamo que vamo!!!')
--- index.js
+++ index.js
@@ -3,12 +3,4 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: `http://localhost:${PORT}`,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Authorization'],
-  }),
-);
-
 app.listen(PORT, () => console.log(`liesten at port ${PORT}`));
