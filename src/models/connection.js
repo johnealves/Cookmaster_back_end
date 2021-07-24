08004b7a1251d99edfd80130f835c3821adafc99
johnealves <johne_af@hotmail.com>
2021-07-23 23:31:02 -0300

    alter connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'john',
-  password: 'Password1@',
-  database: 'osf898i7seo88lnz',
-});
-
 // const connection = mysql.createPool({
-//   host: 'kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//   user: 'lqh2abef9aj30ls7',
-//   password: 'aytq3xlj9xea43vg',
+//   host: 'localhost',
+//   user: 'john',
+//   password: 'Password1@',
 //   database: 'osf898i7seo88lnz',
-// })
+// });
+
+const connection = mysql.createPool({
+  host: 'kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'lqh2abef9aj30ls7',
+  password: 'aytq3xlj9xea43vg',
+  database: 'osf898i7seo88lnz',
+})
 
 module.exports = connection;
